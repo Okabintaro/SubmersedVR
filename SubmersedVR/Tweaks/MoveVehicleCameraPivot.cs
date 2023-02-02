4c7c4d585b99a145d2cf0af4c1bca822e9a90f05
Okabintaro <103938900+Okabintaro@users.noreply.github.com>
2023-02-02 12:12:36 +0100

    Tweak the seat position of seamoth and exosuit
    They are 10cm more forward/infront so you don't clip into the back of the seat that much.

---
+++ SubmersedVR/Tweaks/MoveVehicleCameraPivot.cs
@@ -0,0 +1,29 @@
+using HarmonyLib;
+using UnityEngine;
+
+namespace SubmersedVR
+{
+
+    // Moves the camera/attach point of the seamoth 10 cm in front
+    [HarmonyPatch(typeof(SeaMoth), nameof(SeaMoth.Start))]
+    static class MoveSeamothCameraPivot
+    {
+        public static void Postfix(SeaMoth __instance)
+        {
+            Mod.logger.LogInfo($"Tweaking position of {__instance.playerPosition.name}");
+            __instance.playerPosition.transform.localPosition = new Vector3(0.0f, 0.0f, 0.2f);
+        }
+    }
+
+    // Moves the camera/attach point of the exosuit 10 cm in front
+    [HarmonyPatch(typeof(Exosuit), nameof(Exosuit.Start))]
+    static class MoveExosuitCameraPivot
+    {
+        public static void Postfix(Exosuit __instance)
+        {
+            Mod.logger.LogInfo($"Tweaking position of {__instance.playerPosition.name}");
+            __instance.playerPosition.transform.localPosition = new Vector3(0.0f, 0.0f, 0.2f);
+        }
+    }
+
+}
